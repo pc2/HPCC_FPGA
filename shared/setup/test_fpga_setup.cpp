e3b182f79c0e4cf17d61affeabe38d12ec6d3d08
Marius Meyer <marius.meyer@upb.de>
2020-05-04 09:46:56 +0200

    Fix Xilinx FPGA setup test crash
    Was caused by multithreaded execution that
    googletest could not handle that easily.

--- shared/setup/test_fpga_setup.cpp
+++ shared/setup/test_fpga_setup.cpp
@@ -12,14 +12,11 @@ TEST (FPGASetup, FindValidPlatformAndDevice) {
     EXPECT_EQ (1, fpga_setup::selectFPGADevice(DEFAULT_PLATFORM, DEFAULT_DEVICE).size());
 }
 
-#ifdef INTEL_FPGA
-
-// TODO: Xilinx throws segfault when doing this. Change test to adapt to behavior
-
 /**
  * Checks if non existing platform leads to an error
  */
 TEST (FPGASetup, FindNonExistingPlatform) {
+    testing::FLAGS_gtest_death_test_style="threadsafe";
     // TODO regex does not work so for now its not tested!
     EXPECT_EXIT(fpga_setup::selectFPGADevice(DEFAULT_PLATFORM + 100, DEFAULT_DEVICE),
                 ::testing::ExitedWithCode(1),
@@ -30,11 +27,11 @@ TEST (FPGASetup, FindNonExistingPlatform) {
  * Checks if non existing device leads to an error
  */
 TEST (FPGASetup, FindNonExistingDevice) {
+    testing::FLAGS_gtest_death_test_style="threadsafe";
     // TODO regex does not work so for now its not tested!
     EXPECT_EXIT(fpga_setup::selectFPGADevice(DEFAULT_PLATFORM, DEFAULT_DEVICE + 100),
                 ::testing::ExitedWithCode(1),
                 ::testing::MatchesRegex(".*"));
 }
 
-#endif
 
