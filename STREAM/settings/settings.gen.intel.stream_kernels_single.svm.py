1c768c6819deb605bf193ac027dbbf1c8a330130
Marius Meyer <marius.meyer@uni-paderborn.de>
2020-11-17 16:38:36 +0100

    Apply 1 suggestion(s) to 1 file(s)

--- STREAM/settings/settings.gen.intel.stream_kernels_single.svm.py
+++ STREAM/settings/settings.gen.intel.stream_kernels_single.svm.py
@@ -6,7 +6,7 @@ def generate_attributes(num_replications, num_global_memory_banks=32):
     round robin scheme using the available global memory banks and the number of
     replications that should be generated (e.g. if a global memory contains multiple banks)
 
-    @param num_replications Number okernel replications
+    @param num_replications Number of kernel replications
     @param num_global_memory_banks Number of global memory banks that should be used for generation
 
     @return Array of strings that contain the attributes for every kernel
