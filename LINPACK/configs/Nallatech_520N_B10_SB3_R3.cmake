f6e822a471668a2985799d71336d7e9799f37ee5
Marius Meyer <mariusme@mail.upb.de>
2021-02-01 12:01:13 +0100

    Add configs with kernel replications

--- LINPACK/configs/Nallatech_520N_B10_SB3_R3.cmake
+++ LINPACK/configs/Nallatech_520N_B10_SB3_R3.cmake
@@ -10,11 +10,11 @@ set(USE_MPI No CACHE BOOL "" FORCE)
 set(USE_SVM No CACHE BOOL "" FORCE)
 set(USE_HBM No CACHE BOOL "" FORCE)
 set(FPGA_BOARD_NAME "p520_max_sg280l" CACHE STRING "" FORCE)
-set(AOC_FLAGS "-fpc -fp-relaxed -no-interleaving=default" CACHE STRING "" FORCE)
+set(AOC_FLAGS "-fpc -fp-relaxed" CACHE STRING "" FORCE)
 
 # LINPACK specific options
 set(DEFAULT_MATRIX_SIZE 1024 CACHE STRING "Default matrix size" FORCE)
-set(GLOBAL_MEM_UNROLL 8 CACHE STRING "Unrolling factor that is used for all loops in the kernels" FORCE)
-set(LOCAL_MEM_BLOCK_LOG 10 CACHE STRING "Used to define the width and height of the block stored in local memory" FORCE)
+set(LOCAL_MEM_BLOCK_LOG 9 CACHE STRING "Used to define the width and height of the block stored in local memory" FORCE)
 set(REGISTER_BLOCK_LOG 3 CACHE STRING "Size of the block that will be manipulated in registers" FORCE)
+set(NUM_REPLICATIONS 3 CACHE STRING "Number of times the matrix multiplication kernel will be replicated" FORCE)
 
