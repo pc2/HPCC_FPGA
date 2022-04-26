ddefbac38169e01d2d478809e96843b2c926be07
Marius Meyer <mariusm@xilinx.com>
2022-04-26 18:09:17 +0100

    Update ACCL cmake scripts for TCP

--- cmake/accl.cmake
+++ cmake/accl.cmake
@@ -1,24 +1,21 @@
 
+# General definitions
 set(ACCL_STACK_TYPE "UDP" CACHE STRING "Network layer used in ACCL")
 set(ACCL_UDP_ETH_IF 0 CACHE STRING "Ethernet interface used. On ETHZ: 0 = switch, 1 = direct")
 set(ACCL_DEVICE_NAME "xcu280-fsvh2892-2L-e" CACHE STRING "Name of the FPGA used on the target platform")
-
-set(ACCL_CCLO_KERNEL_DIR ${extern_accl_SOURCE_DIR}/kernels/cclo/)
-set(ACCL_CCLO_KERNEL_XO ccl_offload.xo)
-
 set(ACCL_HARDWARE_DIR ${extern_accl_SOURCE_DIR}/test/hardware)
+
+# UDP related definitions
 set(ACCL_VNX_DIR ${ACCL_HARDWARE_DIR}/xup_vitis_network_example/)
 set(ACCL_NETLAYER_HLS ${ACCL_VNX_DIR}/NetLayers/100G-fpga-network-stack-core)
 set(ACCL_UDP_MAC_XO ${ACCL_VNX_DIR}/Ethernet/_x.${FPGA_BOARD_NAME}/cmac_${ACCL_UDP_ETH_IF}.xo)
 set(ACCL_UDP_NET_XO ${ACCL_VNX_DIR}/NetLayers/_x.${FPGA_BOARD_NAME}/networklayer.xo)
-
 set(ACCL_HLS_IP_FOLDER ${ACCL_NETLAYER_HLS}/synthesis_results_HMB)
-list(APPEND ACCL_LINK_CONFIG --advanced.param compiler.userPostSysLinkOverlayTcl=${ACCL_VNX_DIR}/Ethernet/post_sys_link.tcl)
-list(APPEND ACCL_LINK_CONFIG --user_ip_repo_paths ${ACCL_HLS_IP_FOLDER})
-add_custom_command(
-    OUTPUT ${ACCL_CCLO_KERNEL_DIR}/${ACCL_CCLO_KERNEL_XO}
-    COMMAND make STACK_TYPE=${ACCL_STACK_TYPE} PLATFORM=${FPGA_BOARD_NAME}
-    WORKING_DIRECTORY ${ACCL_CCLO_KERNEL_DIR})
+if (ACCL_STACK_TYPE STREQUAL "UDP")
+    list(APPEND ACCL_LINK_CONFIG --advanced.param compiler.userPostSysLinkOverlayTcl=${ACCL_VNX_DIR}/Ethernet/post_sys_link.tcl)
+    list(APPEND ACCL_LINK_CONFIG --user_ip_repo_paths ${ACCL_HLS_IP_FOLDER})
+    set(ACCL_CCLO_BUILD_ARGS STACK_TYPE=${ACCL_STACK_TYPE})
+endif()
 
 add_custom_command(
     OUTPUT ${ACCL_UDP_MAC_XO}
@@ -30,23 +27,66 @@ add_custom_command(
     COMMAND make -C ${ACCL_VNX_DIR}/NetLayers DEVICE=${FPGA_BOARD_NAME} all
     WORKING_DIRECTORY ${ACCL_HARDWARE_DIR}) 
 
+add_custom_target(
+    accl_udp_stack
+    DEPENDS ${ACCL_UDP_MAC_XO} ${ACCL_UDP_NET_XO})
 
+# TCP related definitions
+set(ACCL_TCP_BASE_DIR ${extern_accl_SOURCE_DIR}/Vitis_with_100Gbps_TCP-IP)
+set(ACCL_TCP_XO ${ACCL_TCP_BASE_DIR}/_x.hw.${FPGA_BOARD_NAME}/network_krnl.xo)
+set(ACCL_TCP_CMAC_XO ${ACCL_TCP_BASE_DIR}/_x.hw.${FPGA_BOARD_NAME}/cmac_krnl.xo)
+if (ACCL_STACK_TYPE STREQUAL "TCP")
+    list(APPEND ACCL_LINK_CONFIG --advanced.param compiler.userPostSysLinkOverlayTcl=${ACCL_TCP_BASE_DIR}/scripts/post_sys_link.tcl)
+    list(APPEND ACCL_LINK_CONFIG --user_ip_repo_paths ${ACCL_TCP_BASE_DIR}/build/fpga-network-stack/iprepo)
+    set(ACCL_CCLO_BUILD_ARGS STACK_TYPE=${ACCL_STACK_TYPE} EN_FANIN=1)
+endif()
+
+# TODO: This is very sppecific to the Xilinx build system, because
+# different Vivado version is required to build these ips
+add_custom_command(
+    OUTPUT ${ACCL_TCP_BASE_DIR}/build/fpga-network-stack/iprepo
+    COMMAND mkdir build && cd build && cmake .. -DFDEV_NAME=u280 
+            -DVIVADO_HLS_ROOT_DIR=/proj/xbuilds/2020.1_released/installs/lin64/Vivado/2020.1 
+            -DVIVADO_ROOT_DIR=/proj/xbuilds/2020.1_released/installs/lin64/Vivado/2020.1 
+            -DTCP_STACK_EN=1 -DTCP_STACK_RX_DDR_BYPASS_EN=1 -DTCP_STACK_WINDOW_SCALING_EN=0
+    WORKING_DIRECTORY ${ACCL_TCP_BASE_DIR})
+
+add_custom_command(
+    OUTPUT ${ACCL_TCP_CMAC_XO}
+    COMMAND make cmac_krnl DEVICE=${FPGA_BOARD_NAME} XSA=${FPGA_BOARD_NAME} TEMP_DIR=_x.hw.${FPGA_BOARD_NAME}/
+    WORKING_DIRECTORY ${ACCL_TCP_BASE_DIR}
+    DEPENDS ${ACCL_TCP_BASE_DIR}/build/fpga-network-stack/iprepo) 
+
+add_custom_command(
+    OUTPUT ${ACCL_TCP_XO}
+    COMMAND make network_krnl DEVICE=${FPGA_BOARD_NAME} XSA=${FPGA_BOARD_NAME} TEMP_DIR=_x.hw.${FPGA_BOARD_NAME}/
+    WORKING_DIRECTORY ${ACCL_TCP_BASE_DIR}
+    DEPENDS ${ACCL_TCP_BASE_DIR}/build/fpga-network-stack/iprepo) 
+
+add_custom_target(
+    accl_tcp_stack
+    DEPENDS ${ACCL_TCP_XO} ${ACCL_TCP_CMAC_XO})
+      
+
+# Build CCLO
+set(ACCL_CCLO_KERNEL_DIR ${extern_accl_SOURCE_DIR}/kernels/cclo/)
+set(ACCL_CCLO_KERNEL_XO ccl_offload.xo)
+
+add_custom_command(
+    OUTPUT ${ACCL_CCLO_KERNEL_DIR}/${ACCL_CCLO_KERNEL_XO}
+    COMMAND make ${ACCL_CCLO_BUILD_ARGS} PLATFORM=${FPGA_BOARD_NAME}
+    WORKING_DIRECTORY ${ACCL_CCLO_KERNEL_DIR})
+
+add_custom_target(
+    accl_cclo
+    DEPENDS ${ACCL_CCLO_KERNEL_DIR}/${ACCL_CCLO_KERNEL_XO})
+
+# Build the ACCL Plugins
 set(ACCL_PLUGINS_DIR ${extern_accl_SOURCE_DIR}/kernels/plugins)
 set(ACCL_PLUGINS_HOSTCTRL ${ACCL_PLUGINS_DIR}/hostctrl/hostctrl.xo)
 set(ACCL_PLUGINS_SUM ${ACCL_PLUGINS_DIR}/reduce_sum/reduce_sum.xo)
 set(ACCL_PLUGINS_COMPRESSION ${ACCL_PLUGINS_DIR}/hp_compression/hp_compression.xo)
 set(ACCL_PLUGINS_LOOPBACK ${ACCL_PLUGINS_DIR}/loopback/loopback.xo)
-
-set(ACCL_UDP_XOS ${ACCL_PLUGINS_LOOPBACK} ${ACCL_PLUGINS_COMPRESSION} ${ACCL_PLUGINS_SUM} ${ACCL_PLUGINS_HOSTCTRL}
-    ${ACCL_CCLO_KERNEL_DIR}/${ACCL_CCLO_KERNEL_XO} ${ACCL_UDP_MAC_XO} ${ACCL_UDP_NET_XO} CACHE INTERNAL "Object files required for ACCL")
-
-add_custom_target(
-    accl_udp_stack
-    DEPENDS ${ACCL_UDP_MAC_XO} ${ACCL_UDP_NET_XO})
-
-add_custom_target(
-    accl_cclo
-    DEPENDS ${ACCL_CCLO_KERNEL_DIR}/${ACCL_CCLO_KERNEL_XO})
 
 add_custom_command(
     OUTPUT ${ACCL_PLUGINS_HOSTCTRL}
@@ -70,7 +110,25 @@ add_custom_target(
     DEPENDS ${ACCL_PLUGINS_LOOPBACK} ${ACCL_PLUGINS_SUM} ${ACCL_PLUGINS_HOSTCTRL} 
     ${ACCL_PLUGINS_COMPRESSION})
 
+set(ACCL_UDP_XOS ${ACCL_PLUGINS_LOOPBACK} ${ACCL_PLUGINS_COMPRESSION} ${ACCL_PLUGINS_SUM} ${ACCL_PLUGINS_HOSTCTRL}
+    ${ACCL_CCLO_KERNEL_DIR}/${ACCL_CCLO_KERNEL_XO} ${ACCL_UDP_MAC_XO} ${ACCL_UDP_NET_XO} CACHE INTERNAL "Object files required for ACCL with UDP")
+
+set(ACCL_TCP_XOS ${ACCL_PLUGINS_LOOPBACK} ${ACCL_PLUGINS_COMPRESSION} ${ACCL_PLUGINS_SUM} ${ACCL_PLUGINS_HOSTCTRL}
+    ${ACCL_CCLO_KERNEL_DIR}/${ACCL_CCLO_KERNEL_XO} ${ACCL_TCP_CMAC_XO} ${ACCL_TCP_XO} CACHE INTERNAL "Object files required for ACCL with TCP")
+
+if (ACCL_STACK_TYPE STREQUAL "UDP")
+    set(ACCL_XOS ${ACCL_UDP_XOS} CACHE INTERNAL "Object files required for ACCL")
+else()
+    set(ACCL_XOS ${ACCL_TCP_XOS} CACHE INTERNAL "Object files required for ACCL")
+endif()
+
 add_custom_target(
     accl_udp)
 add_dependencies(accl_udp accl_udp_stack accl_cclo accl_plugins)
+
+add_custom_target(
+    accl_tcp)
+add_dependencies(accl_tcp accl_tcp_stack accl_cclo accl_plugins)
+
+
 
