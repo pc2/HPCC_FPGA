64ca17851299b7a0d782a13a8d8f0b7773bcec3e
Marius Meyer <mariusm@xilinx.com>
2022-05-04 11:45:37 +0100

    Reformatting and fix data read back

--- PTRANS/src/host/execution_types/execution_xrt_pcie_pq.hpp
+++ PTRANS/src/host/execution_types/execution_xrt_pcie_pq.hpp
@@ -37,32 +37,37 @@ namespace fpga_execution {
 namespace pcie_pq {
 
 /**
- * @brief Transpose and add the matrices using the OpenCL kernel using a PQ distribution and
- * PCIe+MPI over the host for communication
+ * @brief Transpose and add the matrices using the OpenCL kernel using a PQ
+ * distribution and PCIe+MPI over the host for communication
  *
  * @param config The progrma configuration
- * @param data data object that contains all required data for the execution on the FPGA
- * @param handler data handler instance that should be used to exchange data between hosts
- * @return std::unique_ptr<transpose::TransposeExecutionTimings> The measured execution times
+ * @param data data object that contains all required data for the execution on
+ * the FPGA
+ * @param handler data handler instance that should be used to exchange data
+ * between hosts
+ * @return std::unique_ptr<transpose::TransposeExecutionTimings> The measured
+ * execution times
  */
-static std::unique_ptr<transpose::TransposeExecutionTimings>
-calculate(const hpcc_base::ExecutionSettings<transpose::TransposeProgramSettings, xrt::device, bool,
-                                             xrt::uuid> &config,
+static std::unique_ptr<transpose::TransposeExecutionTimings> calculate(
+    const hpcc_base::ExecutionSettings<transpose::TransposeProgramSettings,
+                                       xrt::device, bool, xrt::uuid> &config,
           transpose::TransposeData<bool> &data,
-          transpose::data_handler::DistributedPQTransposeDataHandler<xrt::device, bool, xrt::uuid>
-              &handler) {
+    transpose::data_handler::DistributedPQTransposeDataHandler<
+        xrt::device, bool, xrt::uuid> &handler) {
   int err;
 
   if (config.programSettings->dataHandlerIdentifier !=
       transpose::data_handler::DataHandlerType::pq) {
-    throw std::runtime_error("Used data handler not supported by execution handler!");
+    throw std::runtime_error(
+        "Used data handler not supported by execution handler!");
   }
 #ifdef USE_SVM
-  throw new std::runtime_error(
-      "SVM not supported in the host implementation of this communication method");
+  throw new std::runtime_error("SVM not supported in the host implementation "
+                               "of this communication method");
 #endif
 #ifdef USE_BUFFER_WRITE_RECT_FOR_A
-  throw new std::runtime_error("Using the Write Rect method is not supported in this host "
+  throw new std::runtime_error(
+      "Using the Write Rect method is not supported in this host "
                                "implementation of this communication method");
 #endif
 
@@ -77,56 +82,59 @@ calculate(const hpcc_base::ExecutionSettings<transpose::TransposeProgramSettings
 
   size_t local_matrix_width = handler.getWidthforRank();
   size_t local_matrix_height = handler.getHeightforRank();
-  size_t local_matrix_width_bytes = local_matrix_width * data.blockSize * sizeof(HOST_DATA_TYPE);
+  size_t local_matrix_width_bytes =
+      local_matrix_width * data.blockSize * sizeof(HOST_DATA_TYPE);
 
   size_t total_offset = 0;
   size_t row_offset = 0;
   // Setup the kernels depending on the number of kernel replications
   for (int r = 0; r < config.programSettings->kernelReplications; r++) {
 
-    // Calculate how many blocks the current kernel replication will need to process.
+    // Calculate how many blocks the current kernel replication will need to
+    // process.
     size_t blocks_per_replication =
-        (local_matrix_height * local_matrix_width / config.programSettings->kernelReplications);
-    size_t blocks_remainder =
-        (local_matrix_height * local_matrix_width) % config.programSettings->kernelReplications;
+        (local_matrix_height * local_matrix_width /
+         config.programSettings->kernelReplications);
+    size_t blocks_remainder = (local_matrix_height * local_matrix_width) %
+                              config.programSettings->kernelReplications;
     if (blocks_remainder > r) {
-      // Catch the case, that the number of blocks is not divisible by the number of kernel
-      // replications
+      // Catch the case, that the number of blocks is not divisible by the
+      // number of kernel replications
       blocks_per_replication += 1;
     }
     if (blocks_per_replication < 1) {
       continue;
     }
     blocksPerReplication.push_back(blocks_per_replication);
-    size_t buffer_size = (blocks_per_replication + local_matrix_width - 1) / local_matrix_width *
-                         local_matrix_width * data.blockSize * data.blockSize;
+    size_t buffer_size = (blocks_per_replication + local_matrix_width - 1) /
+                         local_matrix_width * local_matrix_width *
+                         data.blockSize * data.blockSize;
     bufferSizeList.push_back(buffer_size);
     bufferStartList.push_back(total_offset);
     bufferOffsetList.push_back(row_offset);
 
     row_offset = (row_offset + blocks_per_replication) % local_matrix_width;
 
-    total_offset += (bufferOffsetList.back() + blocks_per_replication) / local_matrix_width *
-                    local_matrix_width;
-
-    int memory_bank_info_a = 0;
-    int memory_bank_info_b = 0;
-    int memory_bank_info_out = 0;
+    total_offset += (bufferOffsetList.back() + blocks_per_replication) /
+                    local_matrix_width * local_matrix_width;
 
     // create the kernels
     xrt::kernel transposeKernel(*config.device, *config.program,
                                 ("transpose0:{transpose0_" + std::to_string(r + 1) + "}").c_str());
 
     xrt::bo bufferA(*config.device, data.A,
-                    data.numBlocks * data.blockSize * data.blockSize * sizeof(HOST_DATA_TYPE),
+                    data.numBlocks * data.blockSize * data.blockSize *
+                        sizeof(HOST_DATA_TYPE),
                     transposeKernel.group_id(0));
-    xrt::bo bufferB(*config.device,&data.B[bufferStartList[r] * data.blockSize * data.blockSize],
+    xrt::bo bufferB(
+        *config.device,
+        &data.B[bufferStartList[r] * data.blockSize * data.blockSize],
                     buffer_size * sizeof(HOST_DATA_TYPE), transposeKernel.group_id(1));
-    // TODO For small matrices, the 4KB alignment might fail for buffer B. Temporary fix seen in
-    // lines below (requires extra copying)
-    // xrt::bo bufferB(*config.device, buffer_size * sizeof(HOST_DATA_TYPE),
-    // transposeKernel.group_id(1)); bufferB.write(data.B + bufferStartList[r] * data.blockSize *
-    // data.blockSize);
+    // TODO For small matrices, the 4KB alignment might fail for buffer B.
+    // Temporary fix seen in lines below (requires extra copying) xrt::bo
+    // bufferB(*config.device, buffer_size * sizeof(HOST_DATA_TYPE),
+    // transposeKernel.group_id(1)); bufferB.write(data.B + bufferStartList[r] *
+    // data.blockSize * data.blockSize);
     xrt::bo bufferA_out(*config.device, buffer_size * sizeof(HOST_DATA_TYPE),
                         transposeKernel.group_id(2));
 
@@ -150,7 +158,8 @@ calculate(const hpcc_base::ExecutionSettings<transpose::TransposeProgramSettings
     auto endTransfer = std::chrono::high_resolution_clock::now();
 
     std::chrono::duration<double> transferTime =
-        std::chrono::duration_cast<std::chrono::duration<double>>(endTransfer - startTransfer);
+        std::chrono::duration_cast<std::chrono::duration<double>>(
+            endTransfer - startTransfer);
 
     MPI_Barrier(MPI_COMM_WORLD);
 
@@ -163,7 +172,8 @@ calculate(const hpcc_base::ExecutionSettings<transpose::TransposeProgramSettings
     // Exchange A data via PCIe and MPI
     handler.exchangeData(data);
 
-    std::copy(data.A, data.A + data.numBlocks * data.blockSize * data.blockSize, data.exchange);
+    std::copy(data.A, data.A + data.numBlocks * data.blockSize * data.blockSize,
+              data.exchange);
     for (int r = 0; r < transposeKernelList.size(); r++) {
       bufferListA[r].sync(XCL_BO_SYNC_BO_TO_DEVICE);
     }
@@ -188,14 +198,15 @@ calculate(const hpcc_base::ExecutionSettings<transpose::TransposeProgramSettings
     std::cout << "Rank " << mpi_rank << ": "
               << "Done i=" << repetition << std::endl;
     std::cout << "Kernel execution time: "
-              << std::chrono::duration_cast<std::chrono::duration<double>>(endCalculation -
-                                                                           startKernelCalculation)
+              << std::chrono::duration_cast<std::chrono::duration<double>>(
+                     endCalculation - startKernelCalculation)
                      .count()
               << "s ("
-              << ((config.programSettings->matrixSize * config.programSettings->matrixSize *
-                   sizeof(HOST_DATA_TYPE) * 3) /
-                  std::chrono::duration_cast<std::chrono::duration<double>>(endCalculation -
-                                                                            startKernelCalculation)
+              << ((config.programSettings->matrixSize *
+                   config.programSettings->matrixSize * sizeof(HOST_DATA_TYPE) *
+                   3) /
+                  std::chrono::duration_cast<std::chrono::duration<double>>(
+                      endCalculation - startKernelCalculation)
                       .count() *
                   1.0e-9)
               << " GB/s)" << std::endl;
@@ -205,12 +216,13 @@ calculate(const hpcc_base::ExecutionSettings<transpose::TransposeProgramSettings
     handler.exchangeData(data);
 
     std::chrono::duration<double> calculationTime =
-        std::chrono::duration_cast<std::chrono::duration<double>>(endCalculation -
-                                                                  startCalculation);
+        std::chrono::duration_cast<std::chrono::duration<double>>(
+            endCalculation - startCalculation);
     calculationTimings.push_back(calculationTime.count());
 
-    std::vector<HOST_DATA_TYPE> tmp_write_buffer(local_matrix_height * local_matrix_width *
-                                                 data.blockSize * data.blockSize);
+    std::vector<HOST_DATA_TYPE> tmp_write_buffer(
+        local_matrix_height * local_matrix_width * data.blockSize *
+        data.blockSize);
 
     startTransfer = std::chrono::high_resolution_clock::now();
 
@@ -224,29 +236,31 @@ calculate(const hpcc_base::ExecutionSettings<transpose::TransposeProgramSettings
                col < local_matrix_width * data.blockSize; col++) {
             data.result[bufferStartList[r] * data.blockSize * data.blockSize +
                         row * local_matrix_width * data.blockSize + col] =
-                tmp_write_buffer[row * local_matrix_width * data.blockSize + col];
+                tmp_write_buffer[row * local_matrix_width * data.blockSize +
+                                 col];
           }
         }
         // Copy remaining buffer
-        std::copy(tmp_write_buffer.begin() + local_matrix_width * data.blockSize * data.blockSize,
+        std::copy(tmp_write_buffer.begin() +
+                      local_matrix_width * data.blockSize * data.blockSize,
                   tmp_write_buffer.begin() + bufferSizeList[r],
-                  &data.result[(bufferStartList[r] + local_matrix_width) * data.blockSize *
-                               data.blockSize]);
+                  &data.result[(bufferStartList[r] + local_matrix_width) *
+                               data.blockSize * data.blockSize]);
       } else {
         bufferListA_out[r].sync(XCL_BO_SYNC_BO_FROM_DEVICE);
-        bufferListA_out[r].read(data.result, bufferSizeList[r] * sizeof(HOST_DATA_TYPE),
-                                bufferStartList[r] * data.blockSize * data.blockSize *
-                                    sizeof(HOST_DATA_TYPE));
+        bufferListA_out[r].read(
+            &data.result[bufferStartList[r] * data.blockSize * data.blockSize]);
       }
     }
     endTransfer = std::chrono::high_resolution_clock::now();
-    transferTime +=
-        std::chrono::duration_cast<std::chrono::duration<double>>(endTransfer - startTransfer);
+    transferTime += std::chrono::duration_cast<std::chrono::duration<double>>(
+        endTransfer - startTransfer);
     transferTimings.push_back(transferTime.count());
   }
 
   std::unique_ptr<transpose::TransposeExecutionTimings> result(
-      new transpose::TransposeExecutionTimings{transferTimings, calculationTimings});
+      new transpose::TransposeExecutionTimings{transferTimings,
+                                               calculationTimings});
 
   return result;
 }
