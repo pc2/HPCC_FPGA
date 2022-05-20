019aa15f9246a2b66728c6a85a022a947c3d8942
Marius Meyer <mariusm@xilinx.com>
2022-05-20 14:56:56 +0100

    Update PCIE config for U280

--- LINPACK/configs/Xilinx_U280_B8_SB3_R2_DDR_PCIE.cmake
+++ LINPACK/configs/Xilinx_U280_B8_SB3_R2_DDR_PCIE.cmake
@@ -14,11 +14,13 @@ set(USE_XRT_HOST Yes CACHE BOOL "" FORCE)
 set(USE_OCL_HOST No CACHE BOOL "" FORCE)
 set(FPGA_BOARD_NAME "xilinx_u280_xdma_201920_3" CACHE STRING "" FORCE)
 set(XILINX_ADDITIONAL_LINK_FLAGS --kernel_frequency 250 CACHE STRING "" FORCE)
-
+set(XILINX_KERNEL_NAMES lu top_update left_update inner_update_mm0 CACHE STRING "" FORCE)
+set(FORCE_FILE_ENDING "cpp" CACHE STRING "" FORCE)
 # LINPACK specific options
 set(DEFAULT_MATRIX_SIZE 1024 CACHE STRING "Default matrix size" FORCE)
 set(LOCAL_MEM_BLOCK_LOG 8 CACHE STRING "Used to define the width and height of the block stored in local memory" FORCE)
 set(REGISTER_BLOCK_LOG 3 CACHE STRING "Size of the block that will be manipulated in registers" FORCE)
+set(REGISTER_BLOCK_MM_LOG 3 CACHE STRING "Size of the block that will be manipulated in registers" FORCE)
 set(NUM_REPLICATIONS 2 CACHE STRING "Number of times the matrix multiplication kernel will be replicated" FORCE)
 
 set(XILINX_COMPILE_SETTINGS_FILE "${CMAKE_SOURCE_DIR}/settings/settings.compile.xilinx.hpl_torus_pcie.ddr.ini" CACHE STRING "Compile settings file" FORCE)
