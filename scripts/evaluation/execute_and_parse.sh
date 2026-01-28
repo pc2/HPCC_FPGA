330dfca2e4dd9117ea7bb897ee27910f1ebf5e63
Gerrit Pape <papeg@mail.upb.de>
2026-01-28 16:57:40 +0100

    call python directly in helper script

--- scripts/evaluation/execute_and_parse.sh
+++ scripts/evaluation/execute_and_parse.sh
@@ -5,7 +5,7 @@
 
 SCRIPT_PATH=$( cd "$(dirname $0)"; pwd -P)
 
-$@ | ${SCRIPT_PATH}/parse_raw_to_csv.py 
+$@ | python ${SCRIPT_PATH}/parse_raw_to_csv.py
 
 # Exit with the exit status of the parse command
 exit $?
